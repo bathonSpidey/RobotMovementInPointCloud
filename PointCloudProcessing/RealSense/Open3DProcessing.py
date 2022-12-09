ff1106f21104afbce448245c8fdb8112272a7ce9
Abir Bhattacharyya <bathonbhats@gmail.com>
2022-12-09 11:18:53 +0100

    Working on open3D to reduuce point cloud in real time

---
+++ PointCloudProcessing/RealSense/Open3DProcessing.py
@@ -0,0 +1,7 @@
+import open3d as o3d
+
+def process():
+    o3d.t.io.RealSenseSensor.list_devices()
+
+if __name__ == "__main__":
+    process()
--- PointCloudProcessing/RealSense/ReadPlyFiles.py
+++ PointCloudProcessing/RealSense/ReadPlyFiles.py
@@ -1,4 +1,3 @@
-import numpy as np
 import open3d
 
 def main():
