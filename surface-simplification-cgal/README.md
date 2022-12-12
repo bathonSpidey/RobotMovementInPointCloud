52f6c3d12c7623f4ee24634e6e688378983ef5f3
Lukaa <lukaminjoraia@gmail.com>
2022-12-12 17:34:45 +0100

    cmake changes, some code changes and readme

---
+++ surface-simplification-cgal/README.md
@@ -0,0 +1,18 @@
+# How to install & run on Windows
+
+* Install vcgpk:
+
+~~~ 
+git clone https://github.com/microsoft/vcpkg
+cd vcpkg
+./bootstrap-vcpkg.bat 
+./vcpkg.exe install yasm-tool:x86-windows
+~~~
+
+* Install CGAL:
+
+~~~
+./vcpkg.exe install cgal
+~~~
+
+
--- surface-simplification-cgal/edge_collapse.cpp
+++ surface-simplification-cgal/edge_collapse.cpp
@@ -16,22 +16,20 @@ namespace SMS = CGAL::Surface_mesh_simplification;
 int main(int argc, char** argv)
 {
 	Surface_mesh mesh;
-	const std::string filename = ("../../meshes/floormeshHighRes.off");
-	std::ifstream is(filename);
-	if (!is || !(is >> mesh))
-	{
-		std::cerr << "Invalid mesh: " << filename << std::endl;
-		return EXIT_FAILURE;
-	}
-	double stop_ratio = 0.1;
-	SMS::Count_ratio_stop_predicate<Surface_mesh> stop(stop_ratio);
-	std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
-	SMS::edge_collapse(
-		mesh,
-		stop);
-	std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
-	std::cout << "\n Time elapsed : " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() << "ms" << std::endl;
-	CGAL::IO::write_polygon_mesh("output.off", mesh, CGAL::parameters::stream_precision(50));
-	std::cin.get();
-	return EXIT_SUCCESS;
+  const std::string filename = CGAL::data_file_path("../meshes/floormeshHighRes.off");
+  std::ifstream is(filename);
+  if(!is || !(is >> mesh))
+  {
+    std::cerr << "Invalid mesh: " << filename << std::endl;
+    return EXIT_FAILURE;
+  }
+  std::chrono::steady_clock::time_point start_time = std::chrono::steady_clock::now();
+  double stop_ratio = 0.1;
+  SMS::Count_ratio_stop_predicate<Surface_mesh> stop(stop_ratio);
+  int r = SMS::edge_collapse(mesh, stop);
+  std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
+  std::cout << "\Done!\n" << r << " edges removed.\n" << mesh.number_of_edges() << " final edges.\n";
+  std::cout << "Time elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count() << "ms" << std::endl;
+  CGAL::IO::write_polygon_mesh("output.off", mesh, CGAL::parameters::stream_precision(17));
+  return EXIT_SUCCESS;
 }
